a9da566ddc90f6a73849bacc6346fa8060e565f9
MorrisIT <124792346+MorrisChalmers@users.noreply.github.com>
2023-09-20 15:02:05 +0200

    Collisiondone
    HEJEJE

--- bouncing_balls/Model.java
+++ bouncing_balls/Model.java
@@ -1,5 +1,7 @@
 package bouncing_balls;
 
+import java.util.List;
+
 /**
  * The physics model.
  * 
@@ -22,8 +24,8 @@ class Model {
 		
 		// Initialize the model with a few balls
 		balls = new Ball[2];
-		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2);
-		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3);
+		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0, 0, 0.2);
+		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0, 0, 0.2);
 	}
 
 	void step(double deltaT) {
@@ -35,30 +37,77 @@ class Model {
 			}
 			if (b.y < b.radius || b.y > areaHeight - b.radius) {
 				b.vy *= -1;
-			}
-			
+			}		
+
+	
+
+			gravity(b);
+
+
 			// compute new position according to the speed of the ball
 			b.x += deltaT * b.vx;
 			b.y += deltaT * b.vy;
+			b.vx += deltaT * b.ax;
+			b.vy += deltaT * b.ay;
+		}
+
+		if (isColliding(balls[0] , balls[1])){
+			double [] polarveolocityball0 = rectToPolar(balls[0].vx, balls[0].vy);
+			double [] polarveolocityball1 = rectToPolar(balls[1].vx, balls[1].vy);
+			double i = 1 * polarveolocityball0[0] + 1 * polarveolocityball1[0];
+			double r = polarveolocityball1[0] - polarveolocityball0[0];
+
+			double v2 = (i - r)/2;
+			double v1 = (i+r)/2;
+			
+			
+			
 		}
 	}
+
+	void gravity(Ball ball) {
+		ball.ay = -9.82;
+	}
+
+	boolean isColliding (Ball ball1, Ball ball2){
+
+		double distance = Math.sqrt(Math.pow(ball1.x - ball2.x, 2) + Math.pow(ball1.y - ball2.y, 2));
+        return distance <= (ball1.radius + ball2.radius);
+	}
 	
+	double[] polarToRect(double r, double theta) {
+		double[] result = new double[2];
+		result[0] = r * Math.cos(theta);
+		result[1] = r * Math.sin(theta);
+		return result;
+	}
+
+	double[] rectToPolar(double x, double y) {
+		double[] result = new double[2];
+		result[0] = Math.sqrt(x*x + y*y);
+		result[1] = Math.atan(y / x);
+		return result;
+	}
+
+
 	/**
 	 * Simple inner class describing balls.
 	 */
 	class Ball {
 		
-		Ball(double x, double y, double vx, double vy, double r) {
+		Ball(double x, double y, double vx, double vy, double ax, double ay, double r) {
 			this.x = x;
 			this.y = y;
 			this.vx = vx;
 			this.vy = vy;
+			this.ax = ax;
+			this.ay = ay;
 			this.radius = r;
 		}
 
 		/**
 		 * Position, speed, and radius of the ball. You may wish to add other attributes.
 		 */
-		double x, y, vx, vy, radius;
+		double x, y, vx, vy, ax, ay, radius;
 	}
 }
